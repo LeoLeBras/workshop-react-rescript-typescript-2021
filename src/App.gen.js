78af80f15f9cb574f785697134dc08d6d1201df1
LeoLeBras <leo.lebrasf@gmail.com>
2021-03-01 22:33:08 +0100

    ✨ Install ReScript

---
+++ src/App.gen.js
@@ -0,0 +1,16 @@
+/** 
+ * @flow strict
+ * @generated from App.res
+ * @nolint
+ */
+/* eslint-disable */
+
+// $FlowExpectedError: Reason checked type sufficiently
+import * as AppBS from './App.bs';
+
+// Type annotated function components are not checked by Flow, but typeof() works.
+const make$$forTypeof = function (_: {||}) : React$Node { return null };
+
+export type Props = {||};
+
+export const make: typeof(make$$forTypeof) = AppBS.make;
