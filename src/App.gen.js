7c129dfdefedd9082561cea078824fa9e9a835e4
LeoLeBras <leo.lebrasf@gmail.com>
2021-03-02 07:56:24 +0100

    🔧 Correct gentype setup

--- src/App.gen.js
+++
@@ -1,6 +0,0 @@
-/* Untyped file generated from App.res by genType. */
-/* eslint-disable */
-
-import * as AppBS from './App.bs';
-
-export const make = AppBS.make;
---
+++ src/App.gen.tsx
@@ -0,0 +1,13 @@
+/* TypeScript file generated from App.res by genType. */
+/* eslint-disable import/first */
+
+
+import * as React from 'react';
+
+// tslint:disable-next-line:no-var-requires
+const AppBS = require('./App.bs');
+
+// tslint:disable-next-line:interface-over-type-literal
+export type Props = {};
+
+export const make: React.ComponentType<{}> = AppBS.make;
