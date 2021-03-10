916798b868042b850f43e9cd0a7f8885b890e2ba
LeoLeBras <leo.lebrasf@gmail.com>
2021-03-10 22:24:31 +0100

    💩 Caca

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { greeting } from './Greeting'
+
 function App() {
-  return <div className="App">Silence is golden</div>
+  return (
+    <div className="App">
+      {/* Poor type inference */}
+      {greeting(1, 2)}
+      {greeting('1', '2')}
+    </div>
+  )
 }
 
 export default App
---
+++ src/Greeting.tsx
@@ -0,0 +1,4 @@
+// @ts-expect-error demonstration to poor type inference
+export function greeting(a, b) {
+  return a + b
+}
